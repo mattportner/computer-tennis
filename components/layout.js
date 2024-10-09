6993bcabf8e896119964a9c380ff16d04c4d7077
Matt Portner <mattportner@gmail.com>
2024-10-09 11:25:38 -0700

    push

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Header from "./header"
 const Layout = ({ children }) => {
   return (
     <main>
-      <div className="mx-auto mt-16 max-w-container-small md:max-w-container-large md:mt-32">
+      <div className="mx-auto mt-16 max-w-container-small md:max-w-container-large md:mt-32 bg-white">
         {/* <Header /> */}
         {children}
         <div className="flex flex-row justify-between my-8">
